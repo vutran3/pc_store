23aafd8c5deefe7c652622737235a7036a7c1742
vutran01122003 <tranducvu234@gmail.com>
2025-10-19 15:30:58 +0700

    feat: implement code base for PC store

---
+++ server/src/test/java/com/pc/store/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.store.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
